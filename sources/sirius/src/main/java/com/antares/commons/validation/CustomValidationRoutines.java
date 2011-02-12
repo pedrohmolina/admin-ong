5cdf386b967bf425ca482ff63e6bb2843e6437d6
otakon <otakon@566f87ed-7663-a5b8-1e60-18e6c9073d6a>
2011-02-12 23:46:13 +0000

    Se agrega validacion para numeros decimales mayores a cero (ABMs de ingreso y actividad)

---
+++ sources/sirius/src/main/java/com/antares/commons/validation/CustomValidationRoutines.java
@@ -0,0 +1,38 @@
+package com.antares.commons.validation;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.validator.Field;
+import org.apache.commons.validator.ValidatorAction;
+import org.apache.commons.validator.util.ValidatorUtils;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+
+import com.antares.commons.util.Utils;
+
+public class CustomValidationRoutines {
+
+	/**
+	 * Validacion de numeros reales mayores a cero
+	 * 
+	 * @param bean
+	 * @param va
+	 * @param field
+	 * @param errors
+	 * @param req
+	 * @return
+	 */
+	public static boolean validatePositiveDouble(Object bean, ValidatorAction va, Field field, ActionMessages errors, HttpServletRequest req) {
+		String strValue = ValidatorUtils.getValueAsString(bean, field.getProperty());
+
+		if (strValue != null) {
+			Double value = Utils.parseDouble(strValue);
+			if (value.doubleValue() <= 0) {
+				errors.add("error", new ActionMessage("errors.positiveDouble", Utils.getMessage(field.getArg(0).getKey())));
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
