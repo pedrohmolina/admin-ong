0ab9d181f6786c7f6934f02a777c26e5d393f316
otakon <otakon@566f87ed-7663-a5b8-1e60-18e6c9073d6a>
2011-02-14 02:35:42 +0000

    Se parametrizaron a base de datos algunas constantes hardcodeadas. Para esto se creo la entidad Parametro y sus correspondientes DAO y Service. Se hicieron serializables las clases del modelo. (correccion, faltaban agregar estas clases)

---
+++ sources/sirius/src/main/java/com/antares/sirius/dao/impl/ParametroDAOImpl.java
@@ -0,0 +1,16 @@
+package com.antares.sirius.dao.impl;
+
+import com.antares.commons.dao.impl.GenericDAOImpl;
+import com.antares.sirius.dao.ParametroDAO;
+import com.antares.sirius.model.Parametro;
+
+/**
+ * Implementacion de la interfaz ParametroDAO.
+ *
+ * @version 1.0.0 Created 13/02/2011 by Julian Martinez
+ * @author <a href:mailto:otakon@gmail.com> Julian Martinez </a>
+ *
+ */
+public class ParametroDAOImpl extends GenericDAOImpl<Parametro> implements ParametroDAO {
+	
+}
---
+++ sources/sirius/src/main/java/com/antares/sirius/service/ParametroService.java
@@ -0,0 +1,52 @@
+package com.antares.sirius.service;
+
+import com.antares.commons.enums.ParametroEnum;
+import com.antares.commons.service.BaseService;
+import com.antares.sirius.model.Parametro;
+
+/**
+ * Servicio que contiene la lógica de negocio de la entidad Parametro.
+ * 
+ * @version 1.0.0 Created 13/02/2011 by Julian Martinez
+ * @author <a href:mailto:otakon@gmail.com>Julian Martinez</a>
+ *
+ */
+public interface ParametroService extends BaseService<Parametro> {
+
+	/**
+	 * Devuelve el valor del parametro a partir de su correspondiente enum
+	 * 
+	 * @param parametroEnum
+	 * @return
+	 */
+	String findValueByEnum(ParametroEnum parametroEnum);
+
+	/**
+	 * Devuelve el id del tipo de ingreso correspondiente a "Financiacion"
+	 * 
+	 * @return
+	 */
+	String findIdTipoIngresoFinanciacion();
+
+	/**
+	 * Devuelve el id del estado de proyecto correspondiente a "Idea"
+	 * 
+	 * @return
+	 */
+	Integer findIdEstadoProyectoIdea();
+
+	/**
+	 * Devuelve el id del estado de actividad correspondiente a "Latente"
+	 * 
+	 * @return
+	 */
+	Integer findIdEstadoActividadLatente();
+
+	/**
+	 * Devuelve el id del estado de actividad correspondiente a "En Progreso"
+	 * 
+	 * @return
+	 */
+	Integer findIdEstadoActividadProgreso();
+
+}
