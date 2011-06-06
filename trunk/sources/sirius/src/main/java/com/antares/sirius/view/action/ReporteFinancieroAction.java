53949a94ea9bd8a3c49d691834d3b116c90bd59c
pnicdelfino@gmail.com <pnicdelfino@gmail.com@566f87ed-7663-a5b8-1e60-18e6c9073d6a>
2011-06-06 02:54:14 +0000

    Carga de filtros para el reporte financiero.

--- trunk/sources/sirius/src/main/java/com/antares/sirius/view/action/ReporteFinancieroAction.java
+++ trunk/sources/sirius/src/main/java/com/antares/sirius/view/action/ReporteFinancieroAction.java
@@ -3,6 +3,10 @@
  */
 package com.antares.sirius.view.action;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +17,25 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.antares.commons.util.ReportUtils;
+import com.antares.commons.util.Utils;
+import com.antares.sirius.filter.ActividadFilter;
+import com.antares.sirius.filter.MetaFilter;
+import com.antares.sirius.filter.ObjetivoEspecificoFilter;
+import com.antares.sirius.filter.ObjetivoGeneralFilter;
+import com.antares.sirius.filter.ProyectoFilter;
+import com.antares.sirius.model.Actividad;
+import com.antares.sirius.model.Meta;
+import com.antares.sirius.model.ObjetivoEspecifico;
+import com.antares.sirius.model.ObjetivoGeneral;
+import com.antares.sirius.model.Proyecto;
+import com.antares.sirius.service.ActividadService;
+import com.antares.sirius.service.MetaService;
+import com.antares.sirius.service.ObjetivoEspecificoService;
+import com.antares.sirius.service.ObjetivoGeneralService;
+import com.antares.sirius.service.ProyectoService;
 import com.antares.sirius.service.ReporteFinancieroService;
+import com.antares.sirius.view.form.ActividadForm;
+import com.antares.sirius.view.form.AsignacionForm;
 import com.antares.sirius.view.form.ReporteFinancieroForm;
 
 /**
@@ -23,6 +45,12 @@ import com.antares.sirius.view.form.ReporteFinancieroForm;
 public class ReporteFinancieroAction extends ReporteAction{
 
 	private ReporteFinancieroService reporteFinancieroService;
+
+	private ProyectoService proyectoService;
+	private ObjetivoGeneralService objetivoGeneralService;
+	private ObjetivoEspecificoService objetivoEspecificoService;
+	private MetaService metaService;
+	private ActividadService actividadService;
 	
 	/**
 	 * Inicializa la pantalla de consulta.
@@ -45,6 +73,11 @@ public class ReporteFinancieroAction extends ReporteAction{
 
 	protected void loadCollections(ReporteFinancieroForm form) {
 		form.setFormatosReporte(ReportUtils.getReportFormatList());
+		form.setProyectos(proyectoService.findAll());
+		form.setObjetivosGenerales(objetivoGeneralService.findAll());
+		form.setObjetivosEspecificos(objetivoEspecificoService.findAll());
+		form.setMetas(metaService.findAll());
+		form.setActividades(actividadService.findAll());
 	}
 	
 	/**
@@ -67,7 +100,151 @@ public class ReporteFinancieroAction extends ReporteAction{
 		return mapping.findForward("null");
 	}
 
+	/**
+	 * Metodo para cargar los posibles objetivos generales de un proyecto especifico
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward cargarComboObjetivoGeneral(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) 
+	throws Exception {
 
+		String id =(String) request.getParameter("idProyecto");
+		Collection<ObjetivoGeneral> lista = null;
+		if (!"".equals(id)) {
+			ObjetivoGeneralFilter filter = this.createObjetivoGeneralFilter(id);
+			lista = objetivoGeneralService.findByFilter(filter);
+		}
+		((ReporteFinancieroForm)form).setObjetivosGenerales(lista);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		for (ObjetivoGeneral objetivoGeneral : lista) {
+			map.put(new Integer(objetivoGeneral.getId()).toString(), objetivoGeneral.getNombre());
+		}
+		
+		sendJSON(response, map);
+		return null;
+
+	}
+	
+	/**
+	 * Metodo para cargar los posibles objetivos especificos de un objetivo general especifico
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward cargarComboObjetivoEspecifico(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) 
+	throws Exception {
+
+		String id =(String) request.getParameter("idObjetivoGeneral");
+		Collection<ObjetivoEspecifico> lista = null;
+		if (!"".equals(id)) {
+			ObjetivoEspecificoFilter filter = this.createObjetivoEspecificoFilter(id);
+			lista = objetivoEspecificoService.findByFilter(filter);
+		}
+		((ReporteFinancieroForm)form).setObjetivosEspecificos(lista);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		for (ObjetivoEspecifico objetivoEspecifico : lista) {
+			map.put(new Integer(objetivoEspecifico.getId()).toString(), objetivoEspecifico.getNombre());
+		}
+		
+		sendJSON(response, map);
+		return null;
+
+	}
+	
+	/**
+	 * Metodo para cargar las posibles metas de una objectivo especifico
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward cargarComboMeta(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) 
+	throws Exception {
+
+		String id =(String) request.getParameter("idObjetivoEspecifico");
+		Collection<Meta> lista = null;
+		if (!"".equals(id)) {
+			MetaFilter filter = this.createMetaFilter(id);
+			lista = metaService.findByFilter(filter);
+		}
+		((ReporteFinancieroForm)form).setMetas(lista);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		for (Meta meta : lista) {
+			map.put(new Integer(meta.getId()).toString(), meta.getNombre());
+		}
+		
+		sendJSON(response, map);
+		return null;
+
+	}
+	
+	/**
+	 * Metodo para cargar las posibles actividades de una meta especifica
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward cargarComboActividad(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) 
+	throws Exception {
+
+		String id =(String) request.getParameter("idMeta");
+		Collection<Actividad> lista = null;
+		if (!"".equals(id)) {
+			ActividadFilter filter = this.createActividadFilter(id);
+			lista = actividadService.findByFilter(filter);
+		}
+		((ReporteFinancieroForm)form).setActividades(lista);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		for (Actividad actividad : lista) {
+			map.put(new Integer(actividad.getId()).toString(), actividad.getNombre());
+		}
+		
+		sendJSON(response, map);
+		return null;
+
+	}
+	
+	private ObjetivoGeneralFilter createObjetivoGeneralFilter(String proyecto) {
+		ObjetivoGeneralFilter filter = new ObjetivoGeneralFilter();
+		filter.setProyecto(proyectoService.findById(Integer.parseInt(proyecto)));
+		return filter;
+	}
+
+	private ObjetivoEspecificoFilter createObjetivoEspecificoFilter(String objetivoGeneral) {
+		ObjetivoEspecificoFilter filter = new ObjetivoEspecificoFilter();
+		filter.setObjetivoGeneral(objetivoGeneralService.findById(Integer.parseInt(objetivoGeneral)));
+		return filter;
+	}
+
+	private MetaFilter createMetaFilter(String objetivoEspecifico) {
+		MetaFilter filter = new MetaFilter();
+		filter.setObjetivoEspecifico(objetivoEspecificoService.findById(Integer.parseInt(objetivoEspecifico)));
+		return filter;
+	}
+	
+	private ActividadFilter createActividadFilter(String meta) {
+		ActividadFilter filter = new ActividadFilter();
+		filter.setMeta(metaService.findById(Integer.parseInt(meta)));
+		return filter;
+	}
+	
+	
 	/**
 	 * @return the reporteFinancieroService
 	 */
@@ -82,6 +259,88 @@ public class ReporteFinancieroAction extends ReporteAction{
 	public void setReporteFinancieroService(
 			ReporteFinancieroService reporteFinancieroService) {
 		this.reporteFinancieroService = reporteFinancieroService;
+	}
+
+
+	/**
+	 * @return the proyectoService
+	 */
+	public ProyectoService getProyectoService() {
+		return proyectoService;
+	}
+
+
+	/**
+	 * @param proyectoService the proyectoService to set
+	 */
+	public void setProyectoService(ProyectoService proyectoService) {
+		this.proyectoService = proyectoService;
+	}
+
+
+	/**
+	 * @return the objetivoGeneralService
+	 */
+	public ObjetivoGeneralService getObjetivoGeneralService() {
+		return objetivoGeneralService;
+	}
+
+
+	/**
+	 * @param objetivoGeneralService the objetivoGeneralService to set
+	 */
+	public void setObjetivoGeneralService(
+			ObjetivoGeneralService objetivoGeneralService) {
+		this.objetivoGeneralService = objetivoGeneralService;
+	}
+
+
+	/**
+	 * @return the objetivoEspecificoService
+	 */
+	public ObjetivoEspecificoService getObjetivoEspecificoService() {
+		return objetivoEspecificoService;
+	}
+
+
+	/**
+	 * @param objetivoEspecificoService the objetivoEspecificoService to set
+	 */
+	public void setObjetivoEspecificoService(
+			ObjetivoEspecificoService objetivoEspecificoService) {
+		this.objetivoEspecificoService = objetivoEspecificoService;
+	}
+
+
+	/**
+	 * @return the metaService
+	 */
+	public MetaService getMetaService() {
+		return metaService;
+	}
+
+
+	/**
+	 * @param metaService the metaService to set
+	 */
+	public void setMetaService(MetaService metaService) {
+		this.metaService = metaService;
+	}
+
+
+	/**
+	 * @return the actividadService
+	 */
+	public ActividadService getActividadService() {
+		return actividadService;
+	}
+
+
+	/**
+	 * @param actividadService the actividadService to set
+	 */
+	public void setActividadService(ActividadService actividadService) {
+		this.actividadService = actividadService;
 	}
 
 	
--- trunk/sources/sirius/src/main/java/com/antares/sirius/view/form/ReporteFinancieroForm.java
+++ trunk/sources/sirius/src/main/java/com/antares/sirius/view/form/ReporteFinancieroForm.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 
 import org.apache.struts.action.ActionForm;
 
+import com.antares.sirius.model.Actividad;
 import com.antares.sirius.model.FormatoReporte;
+import com.antares.sirius.model.Meta;
+import com.antares.sirius.model.ObjetivoEspecifico;
+import com.antares.sirius.model.ObjetivoGeneral;
 import com.antares.sirius.model.Proveedor;
+import com.antares.sirius.model.Proyecto;
 
 /**
  * Formulario que contendra los filtros a partir de los cuales se generara el reporte de Finanzas.
@@ -20,8 +25,207 @@ public class ReporteFinancieroForm extends ActionForm{
 	private Collection<FormatoReporte> formatosReporte;
 	private String formatoReporte;
 	
-	//TODO - Definir valores necesarios para este formulario
+	private Collection<Proyecto> proyectos;
+	private Collection<ObjetivoGeneral> objetivosGenerales;
+	private Collection<ObjetivoEspecifico> objetivosEspecificos;
+	private Collection<Meta> metas;
+	private Collection<Actividad> actividades;
 	
+	private String idProyecto;
+	private String idObjetivoGeneral;
+	private String idObjetivoEspecifico;
+	private String idMeta;
+	private String idActividad;
+	
+	private String filtroIdProyecto;
+	private String filtroIdObjetivoGeneral;
+	private String filtroIdObjetivoEspecifico;
+	private String filtroIdMeta;
+	private String filtroIdActividad;
+	
+
+	
+	/**
+	 * @return the proyectos
+	 */
+	public Collection<Proyecto> getProyectos() {
+		return proyectos;
+	}
+	/**
+	 * @param proyectos the proyectos to set
+	 */
+	public void setProyectos(Collection<Proyecto> proyectos) {
+		this.proyectos = proyectos;
+	}
+	/**
+	 * @return the objetivosGenerales
+	 */
+	public Collection<ObjetivoGeneral> getObjetivosGenerales() {
+		return objetivosGenerales;
+	}
+	/**
+	 * @param objetivosGenerales the objetivosGenerales to set
+	 */
+	public void setObjetivosGenerales(Collection<ObjetivoGeneral> objetivosGenerales) {
+		this.objetivosGenerales = objetivosGenerales;
+	}
+	/**
+	 * @return the objetivosEspecificos
+	 */
+	public Collection<ObjetivoEspecifico> getObjetivosEspecificos() {
+		return objetivosEspecificos;
+	}
+	/**
+	 * @param objetivosEspecificos the objetivosEspecificos to set
+	 */
+	public void setObjetivosEspecificos(
+			Collection<ObjetivoEspecifico> objetivosEspecificos) {
+		this.objetivosEspecificos = objetivosEspecificos;
+	}
+	/**
+	 * @return the metas
+	 */
+	public Collection<Meta> getMetas() {
+		return metas;
+	}
+	/**
+	 * @param metas the metas to set
+	 */
+	public void setMetas(Collection<Meta> metas) {
+		this.metas = metas;
+	}
+	/**
+	 * @return the actividades
+	 */
+	public Collection<Actividad> getActividades() {
+		return actividades;
+	}
+	/**
+	 * @param actividades the actividades to set
+	 */
+	public void setActividades(Collection<Actividad> actividades) {
+		this.actividades = actividades;
+	}
+	/**
+	 * @return the idProyecto
+	 */
+	public String getIdProyecto() {
+		return idProyecto;
+	}
+	/**
+	 * @param idProyecto the idProyecto to set
+	 */
+	public void setIdProyecto(String idProyecto) {
+		this.idProyecto = idProyecto;
+	}
+	/**
+	 * @return the idObjetivoGeneral
+	 */
+	public String getIdObjetivoGeneral() {
+		return idObjetivoGeneral;
+	}
+	/**
+	 * @param idObjetivoGeneral the idObjetivoGeneral to set
+	 */
+	public void setIdObjetivoGeneral(String idObjetivoGeneral) {
+		this.idObjetivoGeneral = idObjetivoGeneral;
+	}
+	/**
+	 * @return the idObjetivoEspecifico
+	 */
+	public String getIdObjetivoEspecifico() {
+		return idObjetivoEspecifico;
+	}
+	/**
+	 * @param idObjetivoEspecifico the idObjetivoEspecifico to set
+	 */
+	public void setIdObjetivoEspecifico(String idObjetivoEspecifico) {
+		this.idObjetivoEspecifico = idObjetivoEspecifico;
+	}
+	/**
+	 * @return the idMeta
+	 */
+	public String getIdMeta() {
+		return idMeta;
+	}
+	/**
+	 * @param idMeta the idMeta to set
+	 */
+	public void setIdMeta(String idMeta) {
+		this.idMeta = idMeta;
+	}
+	/**
+	 * @return the idActividad
+	 */
+	public String getIdActividad() {
+		return idActividad;
+	}
+	/**
+	 * @param idActividad the idActividad to set
+	 */
+	public void setIdActividad(String idActividad) {
+		this.idActividad = idActividad;
+	}
+	/**
+	 * @return the filtroIdProyecto
+	 */
+	public String getFiltroIdProyecto() {
+		return filtroIdProyecto;
+	}
+	/**
+	 * @param filtroIdProyecto the filtroIdProyecto to set
+	 */
+	public void setFiltroIdProyecto(String filtroIdProyecto) {
+		this.filtroIdProyecto = filtroIdProyecto;
+	}
+	/**
+	 * @return the filtroIdObjetivoGeneral
+	 */
+	public String getFiltroIdObjetivoGeneral() {
+		return filtroIdObjetivoGeneral;
+	}
+	/**
+	 * @param filtroIdObjetivoGeneral the filtroIdObjetivoGeneral to set
+	 */
+	public void setFiltroIdObjetivoGeneral(String filtroIdObjetivoGeneral) {
+		this.filtroIdObjetivoGeneral = filtroIdObjetivoGeneral;
+	}
+	/**
+	 * @return the filtroIdObjetivoEspecifico
+	 */
+	public String getFiltroIdObjetivoEspecifico() {
+		return filtroIdObjetivoEspecifico;
+	}
+	/**
+	 * @param filtroIdObjetivoEspecifico the filtroIdObjetivoEspecifico to set
+	 */
+	public void setFiltroIdObjetivoEspecifico(String filtroIdObjetivoEspecifico) {
+		this.filtroIdObjetivoEspecifico = filtroIdObjetivoEspecifico;
+	}
+	/**
+	 * @return the filtroIdMeta
+	 */
+	public String getFiltroIdMeta() {
+		return filtroIdMeta;
+	}
+	/**
+	 * @param filtroIdMeta the filtroIdMeta to set
+	 */
+	public void setFiltroIdMeta(String filtroIdMeta) {
+		this.filtroIdMeta = filtroIdMeta;
+	}
+	/**
+	 * @return the filtroIdActividad
+	 */
+	public String getFiltroIdActividad() {
+		return filtroIdActividad;
+	}
+	/**
+	 * @param filtroIdActividad the filtroIdActividad to set
+	 */
+	public void setFiltroIdActividad(String filtroIdActividad) {
+		this.filtroIdActividad = filtroIdActividad;
+	}
 	/**
 	 * @return the formatosReporte
 	 */
