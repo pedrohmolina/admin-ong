4ea284d6c95e4a1a4ef3c4f6147ee24ce87bf187
otakon <otakon@566f87ed-7663-a5b8-1e60-18e6c9073d6a>
2011-08-08 13:05:48 +0000

    Cambio de máscara para que soporte separadores de miles

--- trunk/sources/sirius/src/main/java/com/antares/sirius/base/Constants.java
+++ trunk/sources/sirius/src/main/java/com/antares/sirius/base/Constants.java
@@ -10,8 +10,9 @@ public interface Constants {
 	String CONFIGURATION_FILE = "configuration.properties";
 	String CONFIGURATION_LOG = "log4j.properties";
 	String DEFAULT_DATE_FORMAT = "dd/MM/yyyy";
-	String DEFAULT_DECIMAL_FORMAT = "#0.00";
+	String DEFAULT_DECIMAL_FORMAT = "#,###,##0.00";
 
 	String ANTARES_LOGO = "/reports/Antares.jpg";
 	String SIRIUS_LOGO = "/reports/Sirius.png";
+
 }
